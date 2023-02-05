b6569435054d15a8fac4285ab1f1aae64a39f805
Shailendra Kumar <Shailendrak086@gmail.com>
2023-02-05 17:10:12 +0530

    Project initiated

---
+++ Asteroid - Neo Stats/Asteroid - Neo Stats/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  Asteroid - Neo Stats
+//
+//  Created by Shailendra Kushwaha on 05/02/23.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+         
+        window = UIWindow(frame: UIScreen.main.bounds)
+        let navigationController = UINavigationController(rootViewController: ViewController())
+        window?.rootViewController = navigationController
+        window?.makeKeyAndVisible()
+        return true
+    }
+}
+
---
+++ Asteroid - Neo Stats/Asteroid - Neo Stats/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Asteroid - Neo Stats
+//
+//  Created by Shailendra Kushwaha on 05/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
+    }
+}
+
