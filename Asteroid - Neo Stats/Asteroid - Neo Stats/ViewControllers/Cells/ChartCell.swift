1bc06dd89e4ff06e9a3deff4493a5fb7d8f01d64
Shailendra Kumar <Shailendrak086@gmail.com>
2023-02-06 18:22:44 +0530

    Fixed Date Sequence in label

--- Asteroid - Neo Stats/Asteroid - Neo Stats/ViewControllers/Cells/ChartCell.swift
+++ Asteroid - Neo Stats/Asteroid - Neo Stats/ViewControllers/Cells/ChartCell.swift
@@ -76,14 +76,25 @@ class ChartCell: UITableViewCell {
     private func setupValues(){
         guard let asteroidInfo else { return }
         lineChartEntry.removeAll()
+        let xAxisLabels = sortDates(array:Array(asteroidInfo.nearEarthObjects.keys))
         var index : Int =  0
-        for (key,value) in asteroidInfo.nearEarthObjects{
-            print(key)
-            print(value.count)
-            let entry = ChartDataEntry(x: Double(index), y: Double(value.count))
-            lineChartEntry.append(entry)
+        for label in xAxisLabels {
+            if let asteroids = asteroidInfo.nearEarthObjects[label] as? [NearEarthObject] {
+                let entry = ChartDataEntry(x: Double(index), y: Double(asteroids.count))
+                lineChartEntry.append(entry)
+            }
             index += 1
         }
+        
+        // MARK: this method doesnt work because keys are dictionary sorted dates appear in jumbeled up sequence
+//        var index : Int =  0
+//        for (key,value) in asteroidInfo.nearEarthObjects{
+//            print(key)
+//            print(value.count)
+////            let entry = ChartDataEntry(x: Double(index), y: Double(value.count))
+////            lineChartEntry.append(entry)
+////            index += 1
+//        }
 
         let line1 = LineChartDataSet(entries: lineChartEntry, label: "Number of asteroids per day")
         line1.colors = [NSUIColor.blue]
@@ -93,12 +104,24 @@ class ChartCell: UITableViewCell {
         data.dataSets = [line1]
         lineChartView.data = data
         
-        let xAxisLabels = Array(asteroidInfo.nearEarthObjects.keys)
+        
         lineChartView.xAxis.valueFormatter = IndexAxisValueFormatter(values:xAxisLabels)
             lineChartView.xAxis.granularity = 1
         lineChartView.setVisibleXRangeMaximum(Double(xAxisLabels.count)/2)
         lineChartView.xAxis.setLabelCount(Int(Double(xAxisLabels.count)/2), force: false)
         lineChartView.animate(xAxisDuration: 2.0)
+    }
+    
+    
+    func sortDates(array:[String]) -> [String] {
+        
+        let df = DateFormatter()
+        df.dateFormat = "yyyy-MM-dd"
+        df.locale = Locale(identifier: "en_IN")
+        df.timeZone = TimeZone(identifier: "UTC")!
+
+        return array.sorted {df.date(from: $0)! < df.date(from: $1)!}
+         //->["2019-1-10", "2015-4-2", "2010-11-21", "2010-2-3"]
     }
 
 }
